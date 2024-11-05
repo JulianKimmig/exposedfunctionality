44eb6147359d22466dfa33c2dcb851add0fda172
JulianKimmig <julian.kimmig@gmx.net>
2024-11-05 09:56:40 +0100

    print position of double named io

--- exposedfunctionality/__init__.py
+++ exposedfunctionality/__init__.py
@@ -21,7 +21,7 @@ from .function_parser.custom_wrapper import (
 from .function_parser import serialize_type
 from .function_parser.types import add_type, SerializedType
 
-__version__ = "0.3.17"
+__version__ = "0.3.18"
 
 __all__ = [
     "function_parser",
